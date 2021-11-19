9fbb8cbecddde8e5fac1cf79e37fa939f04acf1c
Xabier Etxezarreta <35383579+xetxezarreta@users.noreply.github.com>
2021-11-19 14:26:02 +0100

    Delete refresh.sh

--- refresh.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-printf "START\n"
-printf "Pulling from Google Drive...\n"
-rclone sync gdrive: /home/xetxezarreta/Documentos/backups/gdrive
-printf "Pushing to Dropbox...\n"
-printf "END\n"
