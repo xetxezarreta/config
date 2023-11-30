45d52240d16187edbbd4dfc4a67efee6a9a24900
Xabier Etxezarreta <35383579+xetxezarreta@users.noreply.github.com>
2023-11-30 18:41:53 +0100

    commit

---
+++ scripts/adguardhome/deploy.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+docker pull adguard/adguardhome
+
+docker run --name adguardhome\
+    --restart unless-stopped\
+    -v /home/ubuntu/adguardhome/workdir:/opt/adguardhome/work\
+    -v /home/ubuntu/adguardhome/confdir:/opt/adguardhome/conf\
+    -p 53:53/tcp -p 53:53/udp\
+    -p 80:80/tcp -p 3000:3000/tcp\
+    -d adguard/adguardhome
